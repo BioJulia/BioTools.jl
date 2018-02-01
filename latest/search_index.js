f4723e26ed231ea354ef9b7bffcb470e8bba1e9d
autodocs <autodocs>
2018-02-01 12:36:25 +0000

    build based on ab16701

---
+++ latest/search_index.js
@@ -0,0 +1,43 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#BioTools.jl-1",
+    "page": "Home",
+    "title": "BioTools.jl",
+    "category": "section",
+    "text": "Latest release:(Image: Latest Release) (Image: BioTools) (Image: License) (Image: ) (Image: BioJulia maintainer: kescobo)Development status: (Image: Build Status) (Image: Build status) (Image: codecov) (Image: )"
+},
+
+{
+    "location": "index.html#Description-1",
+    "page": "Home",
+    "title": "Description",
+    "category": "section",
+    "text": "BioTools.jl provides DNA, RNA and amino acid sequence data types for the julia language, with a comprehensive set of methods for common operations and IO of major sequence data formats."
+},
+
+{
+    "location": "index.html#Installation-1",
+    "page": "Home",
+    "title": "Installation",
+    "category": "section",
+    "text": "Install BioTools from the Julia REPL:julia> Pkg.add(\"BioTools\")If you are interested in the cutting edge of the development, please check out the master branch to try new features before release."
+},
+
+{
+    "location": "index.html#Contributing-and-Questions-1",
+    "page": "Home",
+    "title": "Contributing and Questions",
+    "category": "section",
+    "text": "We appreciate contributions from users including reporting bugs, fixing issues, improving performance and adding new features. Please go to the contributing section of the documentation for more information.If you have a question about contributing or using this package, you are encouraged to use the Bio category of the Julia discourse site."
+},
+
+]}
